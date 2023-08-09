7570e3dee9161f2ba0a9b56b5dac7f3ea827eda4
dezhizhang <1541609448@qq.com>
2023-08-09 22:59:12 +0800

    feat:更新名称

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -1,3 +1,12 @@
+/*
+ * :file description: 
+ * :name: /chatgpt/app/components/home.tsx
+ * :author: 张德志
+ * :copyright: (c) 2023, Tungee
+ * :date created: 2023-08-09 04:46:11
+ * :last editor: 张德志
+ * :date last edited: 2023-08-09 22:54:31
+ */
 "use client";
 
 require("../polyfill");
